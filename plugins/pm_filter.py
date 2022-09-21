73e94079b5421dd1ad4fd14906ace54da9eb3386
Hansaka Anuhas <113702754+SLHansakaAnuhas@users.noreply.github.com>
2022-09-21 17:25:23 +0530

    SLFilmsBot

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -474,7 +474,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await query.message.edit_text(
-            text=script.BUTTON_TXT,
+            text=script.BUTTON_TXT.format(temp.U_NAME),
             reply_markup=reply_markup,
             parse_mode=enums.ParseMode.HTML
         )
