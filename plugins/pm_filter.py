9ae867e7c2014949d5b9d8511727f05198b9b899
Hansaka Anuhas <113702754+SLHansakaAnuhas@users.noreply.github.com>
2022-09-30 10:26:43 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -90,24 +90,15 @@ async def next_page(bot, query):
         off_set = offset - 10
     if n_offset == 0:
         btn.append(
-            [InlineKeyboardButton("🤖 Check Bot PM!", url=f"https://t.me/{temp.U_NAME}")]
-        )
-        btn.append(
             [InlineKeyboardButton("⏪ BACK", callback_data=f"next_{req}_{key}_{off_set}"),
              InlineKeyboardButton(f"📃 Pages {math.ceil(int(offset) / 10) + 1} / {math.ceil(total / 10)}",
                                   callback_data="pages")]
         )
     elif off_set is None:
         btn.append(
-            [InlineKeyboardButton("🤖 Check Bot PM!", url=f"https://t.me/{temp.U_NAME}")]
-        )
-        btn.append(
             [InlineKeyboardButton(f"🗓 {math.ceil(int(offset) / 10) + 1} / {math.ceil(total / 10)}", callback_data="pages"),
              InlineKeyboardButton("NEXT ⏩", callback_data=f"next_{req}_{key}_{n_offset}")])
     else:
-        btn.append(
-            [InlineKeyboardButton("🤖 Check Bot PM!", url=f"https://t.me/{temp.U_NAME}")]
-        )
         btn.append(
             [
                 InlineKeyboardButton("⏪ BACK", callback_data=f"next_{req}_{key}_{off_set}"),
@@ -607,16 +598,10 @@ async def auto_filter(client, msg, spoll=False):
         BUTTONS[key] = search
         req = message.from_user.id if message.from_user else 0
         btn.append(
-            [InlineKeyboardButton("🤖 Check Bot PM!", url=f"https://t.me/{temp.U_NAME}")]
-        )
-        btn.append(
             [InlineKeyboardButton(text=f"🗓 1/{math.ceil(int(total_results) / 10)}", callback_data="pages"),
              InlineKeyboardButton(text="NEXT ⏩", callback_data=f"next_{req}_{key}_{offset}")]
         )
     else:
-        btn.append(
-            [InlineKeyboardButton("🤖 Check Bot PM!", url=f"https://t.me/{temp.U_NAME}")]
-        )
         btn.append(
             [InlineKeyboardButton(text="🗓 1/1", callback_data="pages")]
         )
