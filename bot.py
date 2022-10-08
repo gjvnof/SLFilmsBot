fea8d77c4f533081ed0f640642517365fcea1ce6
Hansaka Anuhas <113702754+gjvnof@users.noreply.github.com>
2022-10-08 15:32:56 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,17 +11,7 @@ from pyrogram import Client, __version__
 from pyrogram.raw.all import layer
 
 
-class Bot(Client):
 
-    def __init__(self):
-        super().__init__(
-            name=SESSION,
-            api_id=API_ID,
-            api_hash=API_HASH,
-            bot_token=BOT_TOKEN,
-            workers=50,
-            plugins={"root": "plugins"},
-            sleep_threshold=5,
         )
 
     async def start(self):
