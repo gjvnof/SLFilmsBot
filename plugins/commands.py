664a216184c6def0f72716d76929ee1646aee548
Hansaka Anuhas <113702754+gjvnof@users.noreply.github.com>
2022-10-08 14:56:06 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -20,10 +20,6 @@ BATCH_FILES = {}
 
 @Client.on_message(filters.command("start") & filters.incoming)
 async def start(client, message):
-    if not await db.is_user_exist(message.from_user.id):
-        await db.add_user(message.from_user.id, message.from_user.first_name)
-        await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
-    if len(message.command) != 2:
         buttons = [[
             InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
             ],[
