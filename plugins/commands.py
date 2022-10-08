cb72a1280e74071217cca6b80512fe15b2daec6d
Hansaka Anuhas <113702754+gjvnof@users.noreply.github.com>
2022-10-08 14:34:49 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -52,4 +52,4 @@ async def start(client, message):
             caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
             reply_markup=reply_markup,
             parse_mode=enums.ParseMode.HTML
-        
+        )
